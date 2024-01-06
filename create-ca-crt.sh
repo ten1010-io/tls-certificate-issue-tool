8620ded1bfbeb22bbff5efa5aa3e5c0005a41ca3
Hyeongdeok Yoon <hyeongdeok.yoon@ten1010.io>
2024-01-07 05:12:07 +0900

    feat: add a days option

--- create-ca-crt.sh
+++ create-ca-crt.sh
@@ -2,22 +2,53 @@
 script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 output_path=$script_path/output
 
+days=3650
+
+function parse_options() {
+  VALID_ARGS=$(getopt -o h --long common-name:,days: -- "$@")
+  if [ $? -ne 0 ]; then
+    echo "error: Fail to parse options" >&2; exit 1
+  fi
+  eval set -- "$VALID_ARGS"
+  while [ : ]; do
+    case "$1" in
+    --common-name)
+      common_name=$2
+      shift 2
+      ;;
+    --days)
+      re='^[0-9]+$'
+      if ! [[ $2 =~ $re ]] ; then
+         echo "Error : --days option value must be number" >&2; exit 1
+      fi
+      days=$2
+      shift 2
+      ;;
+    -h)
+      echo "usage: $(basename $0) [--common-name(required)] [--days(optional)]" >&2; exit 1
+      ;;
+    --)
+      shift
+      break
+      ;;
+    esac
+  done
+}
+
+parse_options "$@"
+
 mkdir -p "$output_path"
 if [ -e "$output_path/ca.crt" ]; then
-  echo "Error : File [$output_path/ca.crt] already exist"
-  exit 1
+  echo "Error : File [$output_path/ca.crt] already exist" >&2; exit 1
 fi
-if [ -z $1 ]; then
-  echo "Error : Common name for certificate must be provided as first argument"
-  exit 1
+if [ -z $common_name ]; then
+  echo "Error : Common name for certificate must be provided" >&2; exit 1
 fi
 if ! cp "${script_path}/ca.conf" "$output_path"; then
-  echo "Error : Writing file failed"
-  exit 1
+  echo "Error : Writing file failed" >&2; exit 1
 fi
-if ! sed -i 's/CN =/''CN = '"$1"'/g' "${output_path}/ca.conf"; then
-  echo "Error : Writing file failed"
-  exit 1
+if ! sed -i 's/CN =/''CN = '"$common_name"'/g' "${output_path}/ca.conf"; then
+  echo "Error : Writing file failed" >&2; exit 1
 fi
 
-openssl req -config "${output_path}/ca.conf" -newkey rsa -x509 -days 3650 -keyout "${output_path}/ca.key" -out "${output_path}/ca.crt" -set_serial 0
+openssl req -config "${output_path}/ca.conf" -newkey rsa -x509 -days $days -keyout "${output_path}/ca.key" -out "${output_path}/ca.crt" -set_serial 0
--- create-tls-crt.sh
+++ create-tls-crt.sh
@@ -2,39 +2,66 @@
 script_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 output_path=$script_path/output
 
-if [ -z $1 ]; then
-  echo "Error : Domain name for tls certificate must be provided as first argument"
-  exit 1
-fi
-if ! echo $1 | grep -P '([a-z0-9A-Z]\.)*[a-z0-9-]+\.([a-z0-9]{2,24})+(\.co\.([a-z0-9]{2,24})|\.([a-z0-9]{2,24}))*'; then
-  echo "Error : Invalid domain name provided"
-  exit 1
-fi
-mkdir -p ${output_path}/$1
-if [ ! -f "$output_path/ca.crt" ]; then
-  echo "Error : File [$output_path/ca.crt] not exist"
-  exit 1
-fi
-if [ -e "${output_path}/$1/tls.crt" ]; then
-  echo "Error : File [${output_path}/$1/tls.crt] already exist"
-  exit 1
-fi
-if ! cp "${script_path}/tls.conf" "${output_path}/$1"; then
-  echo "Error : Writing file failed"
-  exit 1
-fi
-if ! cp "${script_path}/tls.ext" "${output_path}/$1"; then
-  echo "Error : Writing file failed"
-  exit 1
-fi
-if ! sed -i 's/CN =/''CN = '"$1"'/g' "${output_path}/$1/tls.conf"; then
-  echo "Error : Writing file failed"
-  exit 1
-fi
-if ! sed -i 's/DNS.1 =/''DNS.1 = '"$1"'/g' "${output_path}/$1/tls.ext"; then
-  echo "Error : Writing file failed"
-  exit 1
+days=365
+
+function parse_options() {
+  VALID_ARGS=$(getopt -o h --long domain-name:,days: -- "$@")
+  if [ $? -ne 0 ]; then
+    echo "error: Fail to parse options" >&2; exit 1
+  fi
+  eval set -- "$VALID_ARGS"
+  while [ : ]; do
+    case "$1" in
+    --domain-name)
+      if ! echo $2 | grep -P '([a-z0-9A-Z]\.)*[a-z0-9-]+\.([a-z0-9]{2,24})+(\.co\.([a-z0-9]{2,24})|\.([a-z0-9]{2,24}))*'; then
+        echo "Error : Invalid domain name provided" >&2; exit 1
+      fi
+      domain_name=$2
+      shift 2
+      ;;
+    --days)
+      re='^[0-9]+$'
+      if ! [[ $2 =~ $re ]] ; then
+         echo "Error : --days option value must be number" >&2; exit 1
+      fi
+      days=$2
+      shift 2
+      ;;
+    -h)
+      echo "usage: $(basename $0) [--domain-name(required)] [--days(optional)]" >&2; exit 1
+      ;;
+    --)
+      shift
+      break
+      ;;
+    esac
+  done
+}
+
+parse_options "$@"
+if [ -z $domain_name ]; then
+  echo "Error : Domain name for tls certificate must be provided" >&2; exit 1
 fi
 
-openssl req -newkey rsa -config "${output_path}/$1/tls.conf" -keyout "${output_path}/$1/tls.key" -out "${output_path}/$1/tls.csr"
-openssl x509 -req -CA "${output_path}/ca.crt" -CAkey "${output_path}/ca.key" -CAserial "${output_path}/.srl" -CAcreateserial -in "${output_path}/$1/tls.csr" -extfile "${output_path}/$1/tls.ext" -out "${output_path}/$1/tls.crt" -days 365
+mkdir -p ${output_path}/$domain_name
+if [ ! -f "$output_path/ca.crt" ]; then
+  echo "Error : File [$output_path/ca.crt] not exist" >&2; exit 1
+fi
+if [ -e "${output_path}/$domain_name/tls.crt" ]; then
+  echo "Error : File [${output_path}/$domain_name/tls.crt] already exist" >&2; exit 1
+fi
+if ! cp "${script_path}/tls.conf" "${output_path}/$domain_name"; then
+  echo "Error : Writing file failed" >&2; exit 1
+fi
+if ! cp "${script_path}/tls.ext" "${output_path}/$domain_name"; then
+  echo "Error : Writing file failed" >&2; exit 1
+fi
+if ! sed -i 's/CN =/''CN = '"$domain_name"'/g' "${output_path}/$domain_name/tls.conf"; then
+  echo "Error : Writing file failed" >&2; exit 1
+fi
+if ! sed -i 's/DNS.1 =/''DNS.1 = '"$domain_name"'/g' "${output_path}/$domain_name/tls.ext"; then
+  echo "Error : Writing file failed" >&2; exit 1
+fi
+
+openssl req -newkey rsa -config "${output_path}/$domain_name/tls.conf" -keyout "${output_path}/$domain_name/tls.key" -out "${output_path}/$domain_name/tls.csr"
+openssl x509 -req -CA "${output_path}/ca.crt" -CAkey "${output_path}/ca.key" -CAserial "${output_path}/.srl" -CAcreateserial -in "${output_path}/$domain_name/tls.csr" -extfile "${output_path}/$domain_name/tls.ext" -out "${output_path}/$domain_name/tls.crt" -days $days
